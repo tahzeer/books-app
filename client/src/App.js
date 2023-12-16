aae9bd393891e8c2dbbe83d5a5ae00ee21f9f2a3
tahzeer <tahzeer10@gmail.com>
2023-12-16 18:16:32 +0530

    create app

--- client/src/App.js
+++ client/src/App.js
@@ -6,13 +6,18 @@ import {
   BrowserRouter,
   Routes,
 } from "react-router-dom";
+import Books from "./pages/Books";
+import Update from "./pages/Update";
+import Add from "./pages/Add";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element />
+          <Route path="/" element={<Books />} />
+          <Route path="/add" element={<Add />} />
+          <Route path="/update" element={<Update />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ client/src/pages/Add.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Add = () => {
+  const [book, setBook] = useState({
+    title: "",
+    desc: "",
+    price: null,
+    cover: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handelChange = (e) => {
+    setBook((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handelClick = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("https://localhost:8800/books", book);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="form">
+      <h1>Add New Book</h1>
+      <input
+        type="text"
+        placeholder="title"
+        onChange={handelChange}
+        name="title"
+      />
+      <input
+        type="text"
+        placeholder="desc"
+        onChange={handelChange}
+        name="desc"
+      />
+      <input
+        type="number"
+        placeholder="price"
+        onChange={handelChange}
+        name="price"
+      />
+      <input
+        type="text"
+        placeholder="cover"
+        onChange={handelChange}
+        name="cover"
+      />
+      <button onClick={handelClick}>Add</button>
+    </div>
+  );
+};
+
+export default Add;
