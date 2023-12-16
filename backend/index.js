5854d2ea04226d0b160de5feb63894123e038c04
tahzeer <tahzeer10@gmail.com>
2023-12-16 13:16:03 +0530

    setup postman

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,36 @@
 import express from "express";
+import mysql from "mysql";
 
 const app = express();
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "test",
+});
+
+app.get("/", (req, res) => {
+  res.json("hello! this is the backend.");
+});
+
+app.get("/books", (req, res) => {
+  const q = "SELECT * FROM books";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+app.post("/books", (req, res) => {
+  const q = "INSERT INTO books (`title`, `desc`, `cover`) VALUES (?)";
+  const values = ["title from backend", "desc from back", "cover from back"];
+
+  db.query(q, [values], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("book has been created!");
+  });
+});
 
 app.listen(8800, () => {
   console.log("Connected to backend!");
